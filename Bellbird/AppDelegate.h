ebfcd93f7d2e378eb145bd5d949e25d6c606ff1b
Mike Cohen <mike3k@gmail.com>
2017-11-03 09:15:54 -0700

    Initial Commit

---
+++ Bellbird/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bellbird
+//
+//  Created by Mike Cohen on 11/3/17.
+//  Copyright © 2017 Mike Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bellbird/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bellbird
+//
+//  Created by Mike Cohen on 11/3/17.
+//  Copyright © 2017 Mike Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
